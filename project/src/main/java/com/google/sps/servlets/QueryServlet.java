89c8c99afec5478fb585df2ed736f860b3dc12a5
karenav <karenavr@gmail.com>
2020-10-05 14:40:01 +0000

    display built in results

--- project/src/main/java/com/google/sps/servlets/QueryServlet.java
+++ project/src/main/java/com/google/sps/servlets/QueryServlet.java
@@ -1,47 +1,47 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.import java.io.IOException;
+// // Copyright 2019 Google LLC
+// //
+// // Licensed under the Apache License, Version 2.0 (the "License");
+// // you may not use this file except in compliance with the License.
+// // You may obtain a copy of the License at
+// //
+// //     https://www.apache.org/licenses/LICENSE-2.0
+// //
+// // Unless required by applicable law or agreed to in writing, software
+// // distributed under the License is distributed on an "AS IS" BASIS,
+// // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// // See the License for the specific language governing permissions and
+// // limitations under the License.import java.io.IOException;
 
-import java.io.IOException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.util.Set;
-import java.util.List;
-import java.util.Collections;
-import java.util.ArrayList;
+// import java.io.IOException;
+// import javax.servlet.annotation.WebServlet;
+// import javax.servlet.http.HttpServlet;
+// import javax.servlet.http.HttpServletRequest;
+// import javax.servlet.http.HttpServletResponse;
+// import java.util.Set;
+// import java.util.List;
+// import java.util.Collections;
+// import java.util.ArrayList;
 
-import com.google.appengine.repackaged.com.google.common.collect.ImmutableSet;
-import com.google.gson.Gson;
+// import com.google.appengine.repackaged.com.google.common.collect.ImmutableSet;
+// import com.google.gson.Gson;
 
-/** A servlet that handles the user query. */
-@WebServlet("/query")
-public final class QueryServlet extends HttpServlet {
+// /** A servlet that handles the user query. */
+// @WebServlet("/query")
+// public final class QueryServlet extends HttpServlet {
 
 
-  @Override
-  public void doGet(final HttpServletRequest request, 
-      final HttpServletResponse response) throws IOException {
-    final int numPlacesToRecommend = 3;
-    final ImmutableSet<Place> fetchedPlaces = PlacesFetcher.fetch();
-    final List<Place> sortedPlaces = new ArrayList<Place>(fetchedPlaces);
-    Collections.sort(sortedPlaces, Places.randomComparator);
-    final int numOfPlacesToShow = Math.min(numPlacesToRecommend, 
-        sortedPlaces.size());
-    response.setContentType("application/json");
-    for (int i = 0; i < numOfPlacesToShow; ++i) {
-      response.getWriter().write(new Gson().toJson(sortedPlaces[i]));
-    }
-  }
-}
+//   @Override
+//   public void doGet(final HttpServletRequest request, 
+//       final HttpServletResponse response) throws IOException {
+//     final int numPlacesToRecommend = 3;
+//     final ImmutableSet<Place> fetchedPlaces = PlacesFetcher.fetch();
+//     final List<Place> sortedPlaces = new ArrayList<Place>(fetchedPlaces);
+//     Collections.sort(sortedPlaces, Places.randomComparator);
+//     final int numOfPlacesToShow = Math.min(numPlacesToRecommend, 
+//         sortedPlaces.size());
+//     response.setContentType("application/json");
+//     for (int i = 0; i < numOfPlacesToShow; ++i) {
+//       response.getWriter().write(new Gson().toJson(sortedPlaces[i]));
+//     }
+//   }
+// }
