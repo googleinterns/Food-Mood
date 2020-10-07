96eeff0851796affb5863332c1e31f8c0d65a06e
karenav <karenavr@gmail.com>
2020-10-07 15:52:53 +0000

    fix lint errors

--- project/src/main/webapp/script.js
+++ project/src/main/webapp/script.js
@@ -1,13 +1,13 @@
 // Copyright 2019 Google LLC
 //
-// Licensed under the Apache License, Version 2.0 (the "License");
+// Licensed under the Apache License, Version 2.0 (the 'License');
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
 //     https://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
+// distributed under the License is distributed on an 'AS IS' BASIS,
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.import java.io.IOException;
@@ -17,9 +17,9 @@
  * results elements in order to display them to the user.
  */
 function fetchFromQuery() {
-  document.getElementById("query-form").style.display = "none";
-  document.getElementById("results").style.display = "block";
-  let placesDiv = document.getElementById("place");
+  document.getElementById('query-form').style.display = 'none';
+  document.getElementById('results').style.display = 'block';
+  let placesDiv = document.getElementById('place');
   fetch('/query').then(response => response.json()).then((places) => {
     places.forEach((singlePlace) => {
       placesDiv.appendChild(createPlaceElement(singlePlace));
@@ -32,26 +32,26 @@ function fetchFromQuery() {
  */
 function createPlaceElement(place) {
   const placeElement = document.createElement('div');
-  placeElement.class = "place-container";
+  placeElement.class = 'place-container';
 
   // Add name
-  let name = document.createElement('li');
+  const name = document.createElement('li');
   name.innerText = place.name;
   placeElement.appendChild(name);
-  placeElement.appendChild(document.createElement("br"));
+  placeElement.appendChild(document.createElement('br'));
 
   // Add link to website
-  let websiteLink = document.createElement('a');
+  const websiteLink = document.createElement('a');
   websiteLink.href = place.website;
   websiteLink.title = place.website;
-  websiteLink.innerHTML = "Restaurant's website";
+  websiteLink.innerHTML = 'Restaurant\'s website';
   placeElement.appendChild(websiteLink);
-  placeElement.appendChild(document.createElement("br"));
+  placeElement.appendChild(document.createElement('br'));
 
   // Add phone number
-  let phone = document.createTextNode("Phone number:" + place.phone);
+  const phone = document.createTextNode('Phone number:' + place.phone);
   placeElement.appendChild(phone);
-  placeElement.appendChild(document.createElement("br"));
+  placeElement.appendChild(document.createElement('br'));
 
   return placeElement;
 }
@@ -61,7 +61,7 @@ function createPlaceElement(place) {
  * different query.
  */
 function tryAgain() {
-  document.getElementById("query-form").style.display = "block";
-  document.getElementById("results").style.display = "none";
-  document.getElementById("place").innerHTML = "";
+  document.getElementById('query-form').style.display = 'block';
+  document.getElementById('results').style.display = 'none';
+  document.getElementById('place').innerHTML = '';
 }
