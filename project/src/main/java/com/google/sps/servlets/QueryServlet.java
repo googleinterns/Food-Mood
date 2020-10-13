2e3fb58225b0fd08b873f63cade5dc2463009f20
karenav <karenavr@gmail.com>
2020-10-13 16:56:01 +0000

    apply PR comments

--- project/src/main/java/com/google/sps/servlets/QueryServlet.java
+++ project/src/main/java/com/google/sps/servlets/QueryServlet.java
@@ -12,6 +12,8 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.import java.io.IOException;
 
+package com.google.sps.servlets;
+
 import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -22,7 +24,9 @@ import javax.servlet.http.HttpServletResponse;
 import com.google.sps.data.Place;
 import com.google.sps.data.Places;
 import com.google.appengine.repackaged.com.google.common.collect.ImmutableList;
+import com.google.common.annotations.VisibleForTesting;
 import com.google.gson.Gson;
+import com.google.sps.data.PlacesFetcher;
 
 /**
  * A servlet that handles the user query. Currently accepts no input, and responds with a list of
@@ -31,17 +35,30 @@ import com.google.gson.Gson;
 @WebServlet("/query")
 public final class QueryServlet extends HttpServlet {
 
+  @VisibleForTesting
+  static final int MAX_NUM_PLACES_TO_RECOMMEND = 3;
+  private static PlacesFetcher fetcher;
+
+
+  @Override
+  public void init() {
+    fetcher = new PlacesFetcher();
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-    int maxNumPlacesToRecommend = 3;
-    ImmutableList<Place> fetchedPlaces = new PlacesFetcher.fetch();
-    //TODO(M1): add call to filterer
-    ImmutableList<Place> sortedPlaces = Places.randomSort(fetchedPlaces);
-    int numPlacesToDisplay = Math.min(maxNumPlacesToRecommend, sortedPlaces.size());
-    List<Place> placesToDisplay = sortedPlaces.stream()
-        .limit(numPlacesToDisplay)
-        .collect(Collectors.toList());
-    response.setContentType("application/json");
-    response.getWriter().write(new Gson().toJson(placesToDisplay));
+    try {
+      //TODO(M1): add call to filterer
+      List<Place> placesToDisplay = Places.randomSort(fetcher.fetch())
+          .stream()
+          .limit(MAX_NUM_PLACES_TO_RECOMMEND)
+          .collect(Collectors.toList());
+      System.out.println("json: " + placesToDisplay); //TODO: delete
+      response.setContentType("application/json");
+      response.getWriter().write(new Gson().toJson(placesToDisplay));
+    }
+    catch(Exception e) {
+      //TODO(M1): handle errors - TBD
+    }
   }
 }
