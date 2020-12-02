e05892001e919a3de960260816a14a047c5d6184
karenav <karenavr@gmail.com>
2020-12-02 15:05:21 +0000

    fix lint error

--- project/src/main/java/com/google/sps/data/Places.java
+++ project/src/main/java/com/google/sps/data/Places.java
@@ -75,6 +75,6 @@ public final class Places {
   private static boolean placeHasNoWebsiteLink(Place place) {
     return Strings.isNullOrEmpty(place.websiteUrl()) && Strings.isNullOrEmpty(place.googleUrl());
   }
-  
+
   private Places() { }
 }
