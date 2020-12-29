9e0c3556ef411c0611879e9a552dd1417a58dd83
tbarnahor <talbarnahor@gmail.com>
2020-12-29 15:09:30 +0000

    linter fixes

--- project/src/main/java/com/google/sps/data/PlacesFetcher.java
+++ project/src/main/java/com/google/sps/data/PlacesFetcher.java
@@ -89,7 +89,7 @@ public class PlacesFetcher {
      *     used for generating the PlaceDetailsRequests sent to Google Places API
      */
     @VisibleForTesting
-    public PlacesFetcher(
+    PlacesFetcher(
             SearchRequestGenerator textSearchRequestGenerator,
             PlaceDetailsRequestGenerator placeDetailsRequestGenerator) {
         this.searchRequestGenerator = textSearchRequestGenerator;
